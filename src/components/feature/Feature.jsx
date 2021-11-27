73db3289962ee1342e06736e484e3dbfc6e30922
serdar.sen <serdar.sen@kron.com.tr>
2021-11-27 17:48:13 +0300

    Add What is GPT-3 Section. Add The Future is Now Section.

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./feature.css";
 
-const CTA = () => {
-  return <div> CTA </div>;
-};
+const Feature = ({ title, text }) => (
+  <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="gpt3__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
-export default CTA;
+export default Feature;
